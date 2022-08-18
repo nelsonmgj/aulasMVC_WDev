ec119b86bc1e4798fec39317f3350a76918cbf98
nelsonmgj <92230547+nelsonmgj@users.noreply.github.com>
2022-08-18 15:01:15 -0300

    Mudança do texto do comentário somente

--- MVC/index.php
+++ MVC/index.php
@@ -2,7 +2,7 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-//Máxima atenção quanto ao detalhe de usar o app começando com maiúscula, só desse jeito funcionou.
+//Máxima atenção quanto ao detalhe de usar o app começando com maiúscula, só desse jeito funcionou!
 use \App\Controller\Pages\Home;
 
 echo Home::getHome();
