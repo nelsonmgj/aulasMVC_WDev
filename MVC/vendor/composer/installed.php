9fa75555235eeeae11624c3da24403a64bd4dc1a
nelsonmgj <92230547+nelsonmgj@users.noreply.github.com>
2022-08-18 11:59:12 -0300

    instalado o composer
    Instalado o composer com o comando de terminal: composer install.

---
+++ MVC/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aulasmvc_wdev/mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '21e99fd6acfdf13141fd5150902f30ba87605e57',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aulasmvc_wdev/mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '21e99fd6acfdf13141fd5150902f30ba87605e57',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
